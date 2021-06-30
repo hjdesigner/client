2bb1d45e2f7175e3f58e88efc5ec39a29fc3f3a2
Henrique Melanda <henrique.melanda@xpi.com.br>
2021-06-30 15:32:29 -0300

    Creating the component heading

---
+++ src/components/Heading/test.tsx
@@ -0,0 +1,36 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import 'jest-styled-components'
+
+import Heading from '.'
+
+describe('<Heading />', () => {
+  it('Should render a white heading by default', () => {
+    renderWithTheme(<Heading>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+  it('Should render a black heading when color is pessed', () => {
+    renderWithTheme(<Heading color="black">Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      color: '#030517'
+    })
+  })
+  it('Should render a heading with a line at the left side', () => {
+    renderWithTheme(<Heading lineLeft>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'border-left': '0.7rem solid #3CD3C1'
+    })
+  })
+  it('Should render a heading with a line at the bottom', () => {
+    renderWithTheme(<Heading lineBottom>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'border-bottom',
+      '0.5rem solid #F231A5',
+      {
+        modifier: '::after'
+      }
+    )
+  })
+})
